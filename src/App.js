fbdcf8e2219f7dbf6b7b2889995945142304cfcf
Rafael Bispo 644673 <RafaelBispoDev@outlook.co>
2023-03-26 12:34:09 -0300

    create components Card, Feedbackstats refatorando code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import {useState} from 'react'
-import Header from "./componets/Header";
 
+import Header from "./componets/Header";
 import FeedbackList from './componets/FeedbackList';
+import FeedbackStats from './componets/FeedbackStats';
 import FeedbackData from "./data/FeedbackData"
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+  const deleteFeedback = (id) => {
+   if (window.confirm('Are you sure you want to delete this feedback')){
+    setFeedback(feedback.filter((item)=> item.id !== id) )
+   }
+  }
   return (
     <>
       <Header/>
       <div className="container">
-      <FeedbackList feedback={feedback}/>
+        <FeedbackStats feedback={feedback}/>
+      <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
      
       </div>
     </>
